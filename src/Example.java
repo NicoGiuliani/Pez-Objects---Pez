1abd535a9419aaef89b57a4057223525682e3c5d
Nico Giuliani <ngiuliani507103@hotmail.com>
2018-03-08 09:24:06 -0800

    Added fill methods

--- src/Example.java
+++ src/Example.java
@@ -4,5 +4,27 @@ public class Example {
         System.out.println("We are making a new PEZ Dispenser:");
         PezDispenser dispenser = new PezDispenser("Darth Vader");
         System.out.printf("The dispenser is %s.\n", dispenser.getCharacterName());
+
+        if (dispenser.isEmpty()) {
+            System.out.println("The Pez dispenser's empty.");
+            dispenser.fill();
+            System.out.println("Now it's been filled though.");
+            System.out.printf("The dispenser now has %s Pez in it.\n", dispenser.getPezCount());
+        } else {
+            System.out.printf("It isn't empty; it has %s Pez still in it.\n", dispenser.getPezCount());
+        }
+
+        for (int i = 0; i < 4; i++) {
+            dispenser.dispense();
+        }
+
+        System.out.println(dispenser.getPezCount());
+        dispenser.fill(4);
+        System.out.println(dispenser.getPezCount());
+
+
+
+
+
     }
 }
--- src/PezDispenser.java
+++ src/PezDispenser.java
@@ -1,9 +1,43 @@
 class PezDispenser {
-    private String characterName;
+    public final static int MAX_PEZ = 12;
+    final private String characterName;
+    private int pezCount;
 
     public PezDispenser(String name) {
         this.characterName = name;
+        pezCount = 0;
     }
 
-    public String getCharacterName() { return characterName; }
+    public void fill() { fill(MAX_PEZ); }
+
+    public void fill(int pezAmount) {
+        if (pezCount + pezAmount <= 12) {
+            pezCount += pezAmount;
+        } else {
+            System.out.println("Too many Pez!");
+        }
+    }
+
+    public boolean dispense() {
+        boolean wasDispensed = false;
+        if (!isEmpty()) {
+            pezCount--;
+            wasDispensed = true;
+        }
+        return wasDispensed;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public int getPezCount() {
+        return pezCount;
+    }
+
 }
+
