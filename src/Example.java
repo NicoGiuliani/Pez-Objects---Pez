0a714c4521e8ee5dda31ff3ad7c995c1f2606d25
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-03-07 20:52:13 -0800

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("We are making a new PEZ Dispenser:");
+        PezDispenser dispenser = new PezDispenser("Darth Vader");
+        System.out.printf("The dispenser is %s.\n", dispenser.getCharacterName());
+    }
+}
---
+++ src/PezDispenser.java
@@ -0,0 +1,9 @@
+class PezDispenser {
+    private String characterName;
+
+    public PezDispenser(String name) {
+        this.characterName = name;
+    }
+
+    public String getCharacterName() { return characterName; }
+}
